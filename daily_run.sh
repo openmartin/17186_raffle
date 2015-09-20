03bcc5fdfeb254f55f329ad20482aeddde36ae80
pengzl <sunshinesmiler@gmail.com>
2015-09-20 15:26:45 +0800

    shell fix

--- daily_run.sh
+++ daily_run.sh
@@ -1,4 +1,4 @@
-export LANG=zh_CN.GB18030
 source /home/ec2-user/raffle/bin/activate
 cd /home/ec2-user/raffle/17186_raffle
+export LANG=zh_CN.GB18030
 python /home/ec2-user/raffle/17186_raffle/raffle.py
